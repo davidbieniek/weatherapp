abbed6697c5b8f128010a67bd05ab7eb8f63568f
MacBook Pro (Dawid) <nasa@MacBook-Dawid.local>
2024-04-05 10:53:16 +0200

    v1

---
+++ main.js
@@ -0,0 +1,15 @@
+let value = document.getElementById("cityValue");
+let button = document.getElementById("but");
+let temp = document.querySelector(".temp");
+
+button.addEventListener("click", () => {
+  let city = value.value;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pl&appid=501f125e82d177cd9010b8c381b5449d&units=metric`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      temp.innerHTML = `${res.main.temp} C`;
+    });
+});
